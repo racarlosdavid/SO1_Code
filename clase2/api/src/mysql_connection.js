3882221859a6db97534819396d94a04099752dbf
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2021-07-28 20:06:38 -0600

    agregado Dockerfile

--- clase2/api/src/mysql_connection.js
+++ clase2/api/src/mysql_connection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var mysqlConnection = mysql.createPool({
-  host     : '0.0.0.0',//<-- USO ESE CUANDO ESTOY CORRIENDO LA API EN MI COMPU -> ESTA CUANDO ESTOY CORRIENDO LA API EN EL CONTENEDOR'192.168.1.7',
+  host     : '0.0.0.0',
   user     : 'root',
   password : 'Tit@nium123_Us@c',
   database : 'so1'
